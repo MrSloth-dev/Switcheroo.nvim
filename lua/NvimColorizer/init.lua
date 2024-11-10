0f3a4f48ac210cba18bf7022f5df669acc1b4137
Joao Barbosa <you@example.com>
2024-11-10 01:56:05 +0000

    update

--- lua/NvimColorizer/init.lua
+++ lua/NvimColorizer/init.lua
@@ -1,30 +1,58 @@
+local M = {}
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
+local previewers = require("telescope.previewers")
+local config = require("telescope.config")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
-local function theme_picker()
+vim.keymap.set("n", "<leader>r", ":so<CR>")
+
+M.select_theme = function(opts)
 	local themes = vim.fn.getcompletion("", "color")
 	pickers
-		.new({}, {
-			prompt_title = "Select Theme",
-			finder = finders.new_table(themes),
-			sorter = require("telescope.config").values.generic_sorter({}),
-			attach_mappings = function(_, map)
-				map("i", "<CR>", function(bufnr)
+		.new(opts, {
+			prompt_title = "ColorSchemes",
+			finder = finders.new_table({
+				results = themes,
+			}),
+			sorter = config.generic_sorter,
+			previewers = previewers.new_buffer_previewer({
+				define_preview = function(self, entry)
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {
+						"local function example()",
+						"    -- This is a sample code for preview",
+						"    local count = 0",
+						"    for i = 1, 10 do",
+						"        count = count + i",
+						'        print("Count:", count)',
+						"    end",
+						"    return count",
+						"end",
+						"",
+						"class Example:",
+						"    def __init__(self):",
+						"        self.value = 42",
+						"",
+						"    def calculate(self):",
+						"        return self.value * 2",
+					})
+					vim.cmd("colorscheme " .. entry.value)
+				end,
+			}),
+			attach_mappings = function(prompt_bufnr, map)
+				actions.select_default:replace(function()
 					local selection = action_state.get_selected_entry()
-					vim.cmd("colorscheme " .. selection[1])
-					-- Code for preview window here
-				end)
-				map("i", "<Esc>", function()
-					actions.close(bufnr)
+					vim.cmd("colorscheme " .. selection.value)
+					actions.close(prompt_bufnr)
 				end)
 				return true
 			end,
 		})
 		:find()
+	local current_colorscheme = vim.g.colors_name
+	vim.cmd("colorscheme " .. current_colorscheme)
 end
 
-return {
-	theme_picker = theme_picker,
-}
+M.select_theme()
+return M
