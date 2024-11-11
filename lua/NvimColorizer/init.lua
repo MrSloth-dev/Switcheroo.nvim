a411b192a9af73dcf2911a6a0180b4c15ac309c9
Joao Barbosa <you@example.com>
2024-11-11 01:20:26 +0000

    Version 0.1
    Alpha Version, only works on installed plugins, only lua code for
    preview, will add option for treesitter syntax highlight in multiple
    languages

--- lua/NvimColorizer/init.lua
+++ lua/NvimColorizer/init.lua
@@ -1,57 +1,85 @@
 local M = {}
+M.version = "0.1"
+
 local pickers = require("telescope.pickers")
+local sorters = require("telescope.sorters")
 local finders = require("telescope.finders")
 local previewers = require("telescope.previewers")
-local config = require("telescope.config")
 local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 
+local show_preview = true
+-- for fast dev
 vim.keymap.set("n", "<leader>r", ":so<CR>")
 
+local preview_code = {
+	"local function example()",
+	"    -- This is a sample code for preview",
+	"    local count = 0",
+	"    for i = 1, 10 do",
+	"        count = count + i",
+	'        print("Count:", count)',
+	"    end",
+	"    return count",
+	"end",
+	"",
+	"class Example:",
+	"    def __init__(self):",
+	"        self.value = 42",
+	"",
+	"    def calculate(self):",
+	"        return self.value * 2",
+}
+
 M.select_theme = function(opts)
+	opts = opts or {}
 	local themes = vim.fn.getcompletion("", "color")
+	local initial_colorscheme = vim.g.colors_name
 	pickers
 		.new(opts, {
 			prompt_title = "ColorSchemes",
 			finder = finders.new_table({
+				entry_maker = function(entry)
+					return { value = entry, display = entry, ordinal = entry }
+				end,
 				results = themes,
 			}),
-			sorter = config.generic_sorter,
-			previewers = previewers.new_buffer_previewer({
+			sorter = sorters.get_fzy_sorter(opts),
+			previewer = show_preview and previewers.new_buffer_previewer({
 				define_preview = function(self, entry)
-					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {
-						"local function example()",
-						"    -- This is a sample code for preview",
-						"    local count = 0",
-						"    for i = 1, 10 do",
-						"        count = count + i",
-						'        print("Count:", count)',
-						"    end",
-						"    return count",
-						"end",
-						"",
-						"class Example:",
-						"    def __init__(self):",
-						"        self.value = 42",
-						"",
-						"    def calculate(self):",
-						"        return self.value * 2",
-					})
+					local bufnr = self.state.bufnr
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, preview_code)
+					vim.api.nvim_buf_set_option(bufnr, "filetype", "lua")
 					vim.cmd("colorscheme " .. entry.value)
 				end,
-			}),
+			}) or nil,
 			attach_mappings = function(prompt_bufnr, map)
-				actions.select_default:replace(function()
+				local function apply_colorscheme()
 					local selection = action_state.get_selected_entry()
-					vim.cmd("colorscheme " .. selection.value)
+					if selection then
+						vim.cmd("colorscheme " .. selection.value)
+					end
+				end
+				actions.select_default:replace(function()
+					apply_colorscheme()
+					actions.close(prompt_bufnr)
+				end)
+				map("i", "<C-c>", function()
+					vim.cmd("colorscheme " .. initial_colorscheme)
+					actions.close(prompt_bufnr)
+				end)
+				map("n", "<C-c>", function()
+					vim.cmd("colorscheme " .. initial_colorscheme)
+					actions.close(prompt_bufnr)
+				end)
+				map("n", "<ESC>", function()
+					vim.cmd("colorscheme " .. initial_colorscheme)
 					actions.close(prompt_bufnr)
 				end)
 				return true
 			end,
 		})
 		:find()
-	local current_colorscheme = vim.g.colors_name
-	vim.cmd("colorscheme " .. current_colorscheme)
 end
 
 M.select_theme()
